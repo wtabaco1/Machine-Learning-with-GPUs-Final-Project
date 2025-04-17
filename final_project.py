5b3ded3efb50ba24f1aeefbaad7da734e9480b5a
CSMVega <134441618+CSMVega@users.noreply.github.com>
2025-04-17 00:26:10 -0700

    Obtained the codes from external CSV files and converted them to a list

--- final_project.py
+++ final_project.py
@@ -22,11 +22,32 @@ def main():
     # 'And so it begins... war...' -Winstar
     dataset = "ASSISTIVETECH_TOTALNEED.csv"
     
+    # These following variables below are used for encoding non-numerical values to fit into the covariance matrix
+
     age_group = "AGEGROUP.csv"
+    ageGroup = pd.read_csv(age_group)
+    ageList = ageGroup["Code"].tolist()
+    #print(ageList) # For debugging
+
     assistive_product = "ASSISTIVETECHPRODUCT.csv"
+    assistiveProd = pd.read_csv(assistive_product)
+    productList = assistiveProd["Code"].tolist()
+    #print(productList) # For debugging
+
     country = "COUNTRY.csv"
+    countryCSV = pd.read_csv(country)
+    countryList = countryCSV["Code"].tolist()
+    #print(countryList) # For debugging
+
     residence_area_type = "RESIDENCEAREATYPE.csv"
+    residenceType = pd.read_csv(residence_area_type)
+    residenceList = residenceType["Code"].tolist()
+    #print(residenceList) # For debugging
+
     sexes = "SEX.csv"
+    sexesCSV = pd.read_csv(sexes)
+    sexList = sexesCSV["Code"].tolist()
+    #print(sexList) # For debugging
 
     # ~~~SECTION 1.1: Reading the CSV file~~~ #
     df = pd.read_csv(dataset) # Reads the .csv file
@@ -46,6 +67,10 @@ def main():
     tempDF = tempDF.drop(columns=emptyCol)  # Drops all the columns that are completely empty
     #print(tempDF.columns) # For debugging
     #print(tempDF) # For debugging
+
+    # Encoding non-numerical values
+
+
 
     newDF = tempDF # Sets the new dataframe to the cleaned dataframe from the temporary dataframe.
     #print(newDF) # For debugging
